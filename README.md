08732f0b26dd928faf619f01ed8c4b97982b0f7e
kilelx <llx.kieran@gmail.com>
2022-02-24 11:22:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,51 @@
+[View live site](https://kilelx.github.io/Tip-Calculator/)
+
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [View live site](https://kilelx.github.io/Advice-Generator/)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS3
+- JavaScript
+
+### What I learned
+
+I didn't face any difficulties on this challenge. Maybe on the svg, because I wanted to animate the different paths.
+
+## Author
+
+- Website - [Kieran LELEUX](http://www.kieran-leleux.com)
+- Frontend Mentor - [@kilelx](https://www.frontendmentor.io/profile/kilelx)
---
+++ app.js
@@ -0,0 +1,57 @@
+const adviceNumber = document.getElementById("number");
+const adviceString = document.getElementById("advice");
+const divider = document.querySelector(".svg");
+
+
+// CHANGING SVG DEPENDING THE DEVICE SIZE
+let deviceScreen = window.matchMedia("(min-width: 768px)");
+
+if (deviceScreen.matches) {
+    divider.innerHTML = `<svg width="444" height="16" xmlns="http://www.w3.org/2000/svg" id="svg-divider"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`;
+} else {
+    divider.innerHTML = `<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg" id="svg-divider"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`;
+}
+
+// const svg = document.getElementById("svg-divider");
+
+function callApi() {
+
+    fetch(`https://api.adviceslip.com/advice`)
+    .then((reponse) => {
+        return reponse.json();
+    })
+    .then((adviceObj) => {
+        getAdvice(adviceObj);
+    })
+}
+
+function getAdvice(adviceObj) {
+
+    adviceNumber.innerText = adviceObj.slip.id;
+    adviceString.innerText = `"` + adviceObj.slip.advice +  `"`;
+
+    document.getElementById("button").setAttribute("disabled", "true");
+
+    document.querySelectorAll("#svg-divider rect").forEach((rect) => {
+        rect.style.opacity = "0.5";
+    })
+
+    setTimeout(() => {
+        document.querySelector("#svg-divider rect:first-child").style.opacity = "1";
+    }, 1000);
+
+    setTimeout(() => {
+        document.querySelector("#svg-divider rect:last-child").style.opacity = "1";
+        document.getElementById("button").removeAttribute("disabled");
+    }, 2000);
+
+}
+
+// ANIMATION WHEN CLICKING ON THE BUTTON
+document.getElementById("button").addEventListener("click", () => {
+    document.querySelector("#button img").style.animation = "throwDice .4s ease-in-out";
+
+    let removeAnimation = setTimeout(() => {
+        document.querySelector("#button img").style.animation = "";
+    }, 400);
+})
