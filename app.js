8ba3fc618704472e70844370efd5ee780830306d
kilelx <80640314+kilelx@users.noreply.github.com>
2022-02-24 11:26:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ if (deviceScreen.matches) {
     divider.innerHTML = `<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg" id="svg-divider"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`;
 }
 
-// const svg = document.getElementById("svg-divider");
-
 function callApi() {
 
     fetch(`https://api.adviceslip.com/advice`)
